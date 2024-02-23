eaf6d09d1887e3fda8a60bc921fce4eb3788ab96
Taliker <86399179+Taliker@users.noreply.github.com>
2024-02-23 00:09:16 -0500

    Added .env

--- serveur/app.js
+++ serveur/app.js
@@ -1,28 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 const cors = require ('cors');
 
-const mongoString = "mongodb+srv://Gabriel:Password12345@cluster0.qm5dbem.mongodb.net/PlanificateurDB";
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var testGabeRouter = require('./routes/testGabe')
+const dbString = process.env.DATABASE_URI
+const port = process.env.PORT || 5000;
 
-var app = express();
+const testGabeRouter = require('./routes/testGabe')
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
 app.use(cors());
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/testGabe', testGabeRouter);
 
 mongoose.connect(mongoString);
@@ -30,6 +21,6 @@ const db = mongoose.connection;
 db.on('connected', () => console.log('connected to database'));
 db.on('error', (error) => console.error(error));
 
-app.listen(3500, () => console.log('server started'));
+app.listen(3000, () => console.log('server started'));
 
 module.exports = app;
