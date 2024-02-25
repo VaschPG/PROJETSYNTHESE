fef3dfd1ff9a2feb3c88ce96b4bb93e02e1387ef
Taliker <86399179+Taliker@users.noreply.github.com>
2024-02-25 02:23:15 -0500

    Prettier code formatting

--- client/src/pages/ExercisePlan/ExercisePlan.tsx
+++ client/src/pages/ExercisePlan/ExercisePlan.tsx
@@ -7,61 +7,65 @@ const NB_EXERCISES = 6;
 const GET_RANDOM_EXERCISES_URL = '/testGabe/getRandomExercises/';
 
 function App() {
-  const [nbExercises, setNbExercises] = useState(NB_EXERCISES);
-  const [exercises, setExercises] = useState(new Array<Exercise>(nbExercises));
+	const [nbExercises, setNbExercises] = useState(NB_EXERCISES);
+	const [exercises, setExercises] = useState(new Array<Exercise>(nbExercises));
 
-  /**
-   * On click event handler for the search button
-   * Calls getNewExercises to fetch exercises from our back end and add them to exercises state object.
-   */
-  async function handleSearchClick(){
-    getNewExercises();
-  }
+	/**
+	 * On click event handler for the search button
+	 * Calls getNewExercises to fetch exercises from our back end and add them to exercises state object.
+	 */
+	async function handleSearchClick() {
+		getNewExercises();
+	}
 
-  /**
-   * Fetch new exercises from our back-end
-   */
-  async function getNewExercises(){
-    try{
-      console.log('fetching from ' + BASE_URL);
-      console.time('fetch-timer');
-      const response = await fetch(BASE_URL + GET_RANDOM_EXERCISES_URL + nbExercises, { method: 'GET' });
-      const data = await response.json();
-      console.log('Successfully fetching in: ')
-      console.timeEnd('fetch-timer');
-      if(response.ok){
-        setExercises(data);
-      }else{
-        console.log('Response not ok')
-      }
-    }catch(error){
-      console.log(error);
-      console.log('catch');
-    }
-  }
+	/**
+	 * Fetch new exercises from our back-end
+	 */
+	async function getNewExercises() {
+		try {
+			console.log('fetching from ' + BASE_URL);
+			console.time('fetch-timer');
+			const response = await fetch(BASE_URL + GET_RANDOM_EXERCISES_URL + nbExercises, {
+				method: 'GET',
+			});
+			const data = await response.json();
+			console.log('Successfully fetching in: ');
+			console.timeEnd('fetch-timer');
+			if (response.ok) {
+				setExercises(data);
+			} else {
+				console.log('Response not ok');
+			}
+		} catch (error) {
+			console.log(error);
+			console.log('catch');
+		}
+	}
 
-  /**
-   * Change the number of exercises to the amount in the nbExercises input when the input text changes.
-   * @param e nbExercises number input
-   */
-  function handleNbExercisesChange(e: React.ChangeEvent<HTMLInputElement>){
-    const inputValue = +e.target.value;
-    if(inputValue != null && inputValue != 0){
-      setNbExercises(inputValue);
-    }else{
+	/**
+	 * Change the number of exercises to the amount in the nbExercises input when the input text changes.
+	 * @param e nbExercises number input
+	 */
+	function handleNbExercisesChange(e: React.ChangeEvent<HTMLInputElement>) {
+		const inputValue = +e.target.value;
+		if (inputValue != null) {
+			setNbExercises(inputValue);
+		}
+	}
 
-    }
-  }
-
-  return (
-    <>
-      <input type='number' placeholder='6' value={nbExercises} onChange={handleNbExercisesChange} min="1" max="10"></input>
-      <button className='button' onClick={ handleSearchClick }>Get new exercises</button>
-      <div className='container card'>
-        { exercises.map( (item, i) => (<ExerciseCard key={ i } exercise={item}/>)) }
-      </div>
-    </>
-  )
+	return (
+		<>
+			<input type='number' placeholder='6' value={nbExercises} onChange={handleNbExercisesChange} min='1' max='10'></input>
+			<button className='button' onClick={handleSearchClick}>
+				Get new exercises
+			</button>
+			<div className='container card'>
+				{exercises.map((item, i) => (
+					<ExerciseCard key={i} exercise={item} />
+				))}
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
--- client/src/pages/ExercisePlan/components/ExerciseCard.tsx
+++ client/src/pages/ExercisePlan/components/ExerciseCard.tsx
@@ -5,33 +5,38 @@ import '../ExercisePlan.css';
  * Interface to ensure type security for our props.
  */
 interface IProps {
-  exercise: Exercise;
+	exercise: Exercise;
 }
 
 const EXERCISE_NAME_LABEL = 'Exercise name: ';
 const EXERCISE_EQUIPMENT_LABEL = 'Equipment required: ';
 const EXERCISE_BODYPART_LABEL = 'Primary muscle group: ';
 
-
-function ExerciseCard( { exercise }: IProps) {
-
-  return (
-    <>
-      <div className='card'>
-        <div className='info'>
-          <div className='exercise-name-div'>
-            <h2 className='exercise-name'>{EXERCISE_NAME_LABEL}</h2>
-            <h2 className='exercise-name'>{exercise.name}</h2>
-          </div>
-          <div>
-            <p>{EXERCISE_EQUIPMENT_LABEL}{exercise.equipment}</p>
-            <p>{EXERCISE_BODYPART_LABEL}{exercise.bodyPart}</p>
-          </div>
-        </div>
-        <img src={exercise.gifUrl} className='image'></img>
-      </div>
-    </>
-  )
+function ExerciseCard({ exercise }: IProps) {
+	return (
+		<>
+			<div className='card'>
+				<div className='card-bodypart-picker'></div>
+				<div className='card-text-div'>
+					<div className='exercise-name-div'>
+						<h2 className='exercise-name'>{EXERCISE_NAME_LABEL}</h2>
+						<h2 className='exercise-name'>{exercise.name}</h2>
+					</div>
+					<div className='exercise-info-div'>
+						<p>
+							{EXERCISE_EQUIPMENT_LABEL}
+							{exercise.equipment}
+						</p>
+						<p>
+							{EXERCISE_BODYPART_LABEL}
+							{exercise.bodyPart}
+						</p>
+					</div>
+				</div>
+				<img src={exercise.gifUrl} className='image'></img>
+			</div>
+		</>
+	);
 }
 
 export { Exercise };
