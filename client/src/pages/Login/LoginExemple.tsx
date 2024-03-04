90c9baede0204d4a570757948509b223458745b8
Dylan-How <doomofpotatos@gmail.com>
2024-03-04 12:19:49 -0500

    Mettre un exemple d'insert profile dans LoginExemplel

--- client/src/pages/Login/LoginExemple.tsx
+++ client/src/pages/Login/LoginExemple.tsx
@@ -1,6 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
+const VITE_API_PROFILE_URL = import.meta.env.VITE_API_PROFILE_URL;
+const VITE_BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+
 // JUST A EXAMPLE TO UNDERSTAND OR TEST HOW TO USE AUTH0
 // CAN DELETE AFTER USING IT IN OTHER PAGES
 const LoginExemple = () => {
@@ -9,15 +12,32 @@ const LoginExemple = () => {
   if (isLoading) {
     return <div>Loading ...</div>;
   }
+  fetch(
+    VITE_BASE_API_URL +
+      VITE_API_PROFILE_URL +
+      "insertId/" +
+      user?.sub?.substring(user?.sub.indexOf("|") + 1),
+    {
+      method: "POST",
+    }
+  )
+    .then((response) => response.text()) // Read response as text
+    .then((data) => console.log(data)); // Alert the response
 
   return (
-    isAuthenticated && (
-      <div>
-        <img src={user?.picture} alt={user?.name} />
-        <h2>{user?.name}</h2>
-        <p>{user?.email}</p>
-      </div>
-    )
+    <>
+      {isAuthenticated && (
+        <div>
+          <img src={user?.picture} alt={user?.name} />
+          <h2>{user?.name}</h2>
+          <p>{user?.email}</p>
+          <div color="black">
+            {user?.sub?.substring(user?.sub.indexOf("|") + 1)}
+          </div>
+        </div>
+      )}
+      {!isAuthenticated && <div>n'est pas logged in</div>}
+    </>
   );
 };
 
--- serveur/app.js
+++ serveur/app.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -11,24 +11,26 @@ const app = express();
 //true=process.env.TEST_DATABASE_URI
 const useTestDB = false;
 
-const dbString = !useTestDB ? process.env.DATABASE_URI : process.env.TEST_DATABASE_URI;
+const dbString = !useTestDB
+  ? process.env.DATABASE_URI
+  : process.env.TEST_DATABASE_URI;
 const port = process.env.PORT || 5000;
 
-const exercisesRouter = require('./routes/exercise_router');
-//const profileRouter = require('./routes/profile_router');
+const exercisesRouter = require("./routes/exercise_router");
+const profileRouter = require("./routes/profile_router");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/exercise/', exercisesRouter);
-//app.use('/api/profile/', profileRouter);
+app.use("/api/exercise/", exercisesRouter);
+app.use("/api/profile/", profileRouter);
 
 mongoose.connect(dbString);
 const db = mongoose.connection;
-db.on('connected', () => console.log('connected to database'));
-db.on('error', (error) => console.error(error));
+db.on("connected", () => console.log("connected to database"));
+db.on("error", (error) => console.error(error));
 
-app.listen(port, () => console.log('server started on port:' + port));
+app.listen(port, () => console.log("server started on port:" + port));
 
 module.exports = app;
