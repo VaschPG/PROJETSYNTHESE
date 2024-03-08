501e36d3b73dd9dc55fbc9b1d7f426253b2fbcd5
VaschPG <137076130+VaschPG@users.noreply.github.com>
2024-03-08 10:16:59 -0500

    petite modification

--- serveur/db/goal_dbo.js
+++ serveur/db/goal_dbo.js
@@ -1,15 +1,16 @@
-const GoalModel = require("../models/goal_model");
+const goalModel = require("../models/goal_model");
 
 module.exports = {
   getAllGoals: async function (userID) {
-    return await GoalModel.find({ userID });
-  },
-  addGoal: async function (userID, goal) {
-    const newGoal = new GoalModel({ userID, goal });
-    return await newGoal.save();
-  },
-  removeGoal: async function (goalID) {
-    return await GoalModel.findByIdAndRemove(goalID);
+    return await goalModel.find({ userID });
   },
 
+  addGoal: async function (userID, goalText) {
+    const newGoal = new goalModel({ userID, text: goalText });
+    return await newGoal.save();
+  },
+
+  removeGoal: async function (goalID) {
+    return await goalModel.findByIdAndRemove(goalID);
+  },
 };
