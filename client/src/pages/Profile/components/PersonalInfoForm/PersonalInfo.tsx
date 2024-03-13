6ffbcd55cf1a629b58e4bfc00650c3fe2115f0a6
Dylan-How <doomofpotatos@gmail.com>
2024-03-13 04:02:07 -0400

    Reformat en bootstrap et ajouter la validation
    Tout le html et css ont changer a bootstrap
    Le fetch de profil a changer aussi, mais ca retourne la meme chose
    Je ne sais pas pq le handlechange ne fonctionnais pas, mais il on tous un handle separe
    Ajout d'un toast

--- client/src/pages/Profile/components/PersonalInfoForm/PersonalInfo.tsx
+++ client/src/pages/Profile/components/PersonalInfoForm/PersonalInfo.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import Toast from 'react-bootstrap/Toast';
+
+
 
 const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 const API_EXERCISES_URL = import.meta.env.VITE_API_PROFILE_URL;
@@ -28,6 +32,8 @@ interface FormData {
 const PersonalInfo: React.FC = () => {
   const { user } = useAuth0();
   const [formData, setFormData] = useState<FormData>(DEFAULT_FORM_DATA);
+  const [validated, setValidated] = useState(false);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     fetchProfile();
@@ -37,6 +43,7 @@ const PersonalInfo: React.FC = () => {
     }*/
   }, []);
 
+  //ne fonctionnait pas, dont know why
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     const numericValue = ["age", "weight", "height"].includes(name) ? parseInt(value, 10) : value;
@@ -53,28 +60,27 @@ const PersonalInfo: React.FC = () => {
     });
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & {
-      firstName: { value: string };
-      lastName: { value: string };
-      age: { value: number };
-      gender: { value: string };
-      weight: { value: number };
-      height: { value: number };
-    };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const target = e.currentTarget;
+      e.preventDefault();
+    if (target.checkValidity() === false){
+      e.stopPropagation();
+    }else{
+      setValidated(true);
+      
+      const data = {
+        _id: user?.sub?.substring(user?.sub.indexOf("|") + 1),
+        firstName: target.firstName.value,
+        lastName: target.lastName.value,
+        age: target.age.value,
+        gender: target.gender.value,
+        weight: target.weight.value,
+        height: target.height.value,
+      };
+      fetchUpdateProfile(data);
+    }
+    
 
-    const data = {
-      _id: user?.sub?.substring(user?.sub.indexOf("|") + 1),
-      firstName: target.firstName.value,
-      lastName: target.lastName.value,
-      age: target.age.value,
-      gender: target.gender.value,
-      weight: target.weight.value,
-      height: target.height.value,
-    };
-
-    fetchUpdateProfile(data);
     /*console.log("Form submitted");
     try {
       localStorage.setItem("userData", JSON.stringify(formData));
@@ -88,6 +94,10 @@ const PersonalInfo: React.FC = () => {
     }*/
   };
 
+  function showToast() {
+    setShow(true);
+  }
+
   async function fetchProfile() {
     try {
       const FETCH_URL = FULL_API_URL + "GetProfile/" + user?.sub?.substring(user?.sub.indexOf("|") + 1);
@@ -98,7 +108,6 @@ const PersonalInfo: React.FC = () => {
       });
       const data = await response.json();
       if (response.ok) {
-        console.log("Successfully fetched in: ");
         setFormData(data);
       } else {
         console.log("Response not ok" + data.message);
@@ -131,6 +140,7 @@ const PersonalInfo: React.FC = () => {
       });
       if (response.ok) {
         console.log("Successfully posted from " + FETCH_URL);
+        showToast();
       } else {
         console.log("Response not ok");
       }
@@ -140,57 +150,133 @@ const PersonalInfo: React.FC = () => {
   }
 
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit} method="post">
-        <div className="form-group">
-          <div className="input-wrap">
-            <label className="label">Prénom :</label>
-            <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} className="input-field" required />
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="input-wrap">
-            <label className="label">Nom :</label>
-            <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} className="input-field" required />
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="input-wrap">
-            <label className="label">Âge :</label>
-            <input type="number" name="age" value={formData.age} onChange={handleInputChange} className="input-field" required />
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="input-wrap">
-            <label className="label">Genre :</label>
-            <select name="gender" value={formData.gender} onChange={handleSelectChange} className="input-field" required>
-              <option value="">Selectionner...</option>
-              <option value="male">Homme</option>
-              <option value="female">Femme</option>
-              <option value="non-binary">Non-Binaire</option>
-              <option value="other">Autre</option>
-              <option value="prefer not to answer">Préfère ne pas répondre</option>
-            </select>
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="input-wrap">
-            <label className="label">Poids (kg):</label>
-            <input type="number" name="weight" value={formData.weight} onChange={handleInputChange} className="input-field" required />
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="input-wrap">
-            <label className="label">Taille (cm):</label>
-            <input type="number" name="height" value={formData.height} onChange={handleInputChange} className="input-field" required />
-          </div>
-        </div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Button type="submit">Sauvegarder</Button>
-        </div>
-      </form>
-      {/*<div className="text-white">{user?.sub?.substring(user?.sub.indexOf("|") + 1)}</div>*/}
-    </div>
+      <>
+      <Container className="position-relative">
+      <Form noValidate validated={validated} onSubmit={handleSubmit} method="post">
+      <Row>
+        <Form.Group as={Col} md="12" controlId="firstName">
+          <Form.Label className="text-white">Prenom :</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            value={formData.firstName}
+            onChange={e => setFormData({ ...formData, ["firstName"]: e.target.value, })}
+            ></Form.Control>
+          <Form.Control.Feedback></Form.Control.Feedback>
+          <Form.Control.Feedback className="text-white" type="invalid">
+            <b>
+            Entrez un prénom.
+            </b>
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} md="12" controlId="lastName">
+          <Form.Label className="text-white">Nom :</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            value={formData.lastName}
+            onChange={e => setFormData({ ...formData, ["lastName"]: e.target.value, })}
+            ></Form.Control>
+          <Form.Control.Feedback></Form.Control.Feedback>
+          <Form.Control.Feedback className="text-white" type="invalid">
+            <b>
+            Entrez un nom.
+            </b>
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} md="12" controlId="age">
+          <Form.Label className="text-white">Âge :</Form.Label>
+          <Form.Control
+            required
+            type="number"
+            min="0"
+            value={formData.age}
+            onChange={e => setFormData({ ...formData, ["age"]: parseInt(e.target.value) })}
+            ></Form.Control>
+          <Form.Control.Feedback></Form.Control.Feedback>
+          <Form.Control.Feedback className="text-white" type="invalid">
+            <b>
+            Entrez un âge.
+            </b>
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} md="12" controlId="gender">
+          <Form.Label className="text-white">Genre :</Form.Label>
+          <Form.Control
+            as="select"
+            required
+            value={formData.gender}
+            onChange={e => setFormData({ ...formData, ["gender"]: e.target.value, })}>
+            <option disabled value="">Selectionner...</option>
+            <option value="male">Homme</option>
+            <option value="female">Femme</option>
+            <option value="non-binary">Non-Binaire</option>
+            <option value="other">Autre</option>
+            <option value="prefer not to answer">Préfère ne pas répondre</option>
+          </Form.Control>
+          <Form.Control.Feedback></Form.Control.Feedback>
+          <Form.Control.Feedback className="text-white" type="invalid">
+            <b>
+            SVP choisir un choix
+            </b>
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} md="12" controlId="weight">
+          <Form.Label className="text-white">Poids (kg):</Form.Label>
+          <Form.Control
+            required
+            type="number"
+            step="0.01"
+            value={formData.weight}
+            onChange={e => setFormData({ ...formData, ["weight"]: parseFloat(e.target.value) })}
+            ></Form.Control>
+          <Form.Control.Feedback></Form.Control.Feedback>
+          <Form.Control.Feedback className="text-white" type="invalid">
+            <b>
+            Entrez votre poids en kg.
+            </b>
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group as={Col} className="mb-3" md="12" controlId="height">
+          <Form.Label className="text-white">Taille (cm):</Form.Label>
+          <Form.Control
+            required
+            type="number"
+            step="0.01"
+            value={formData.height}
+            onChange={e => setFormData({ ...formData, ["height"]: parseFloat(e.target.value) })}
+            ></Form.Control>
+          <Form.Control.Feedback></Form.Control.Feedback>
+          <Form.Control.Feedback className="text-white" type="invalid">
+            <b>
+            Entrez votre taille en cm.
+            </b>
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+    {/* TOAST */}
+      <Toast onClose={() => setShow(false)} show={show} delay={4000} bg="info" autohide className="position-absolute end-0 bottom-0">
+          <Toast.Header>
+            <strong className="me-auto">User update</strong>
+            <small>maintenant</small>
+          </Toast.Header>
+          <Toast.Body>Les informations de {formData.firstName} {formData.lastName} ont été sauvegardées!</Toast.Body>
+        </Toast>
+      <Button className="btn-info" type="submit">Sauvegarder
+      </Button>
+    </Form>
+      </Container>
+    </>
   );
 };
 
