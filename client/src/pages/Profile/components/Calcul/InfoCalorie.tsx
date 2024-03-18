7c2c7c611dab469a11fda52a603037e32c6985d5
Dylan-How <doomofpotatos@gmail.com>
2024-03-18 08:34:00 -0400

    update les titres dans calculs

--- client/src/pages/Profile/components/Calcul/InfoCalorie.tsx
+++ client/src/pages/Profile/components/Calcul/InfoCalorie.tsx
@@ -26,17 +26,28 @@ const InfoCalorie = () => {
       >
         <Image src={infoimg} height="20" width="20" />
       </Button>
-      <Modal show={show} onHide={handleClose} centered dialogClassName="modal-50w">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        centered
+        dialogClassName="modal-50w"
+      >
         <Modal.Header closeButton>
-          <Modal.Title>Infomations sur IMC</Modal.Title>
+          <Modal.Title>Infomations sur les calories</Modal.Title>
         </Modal.Header>
         <Modal.Body className="text-dark">
           <p className="text-dark">
-            Le calcul des calories peut être utilisé pour estimer le nombre de calories qu'une personne doit consommer chaque jour. Ce calcul peut
-            également fournir quelques directives simples pour prendre ou perdre du poids
+            Le calcul des calories peut être utilisé pour estimer le nombre de
+            calories qu'une personne doit consommer chaque jour. Ce calcul peut
+            également fournir quelques directives simples pour prendre ou perdre
+            du poids
           </p>
-          <p className="text-dark">For men: BMR = 13.397W + 4.799H - 5.677A + 88.362</p>
-          <p className="text-dark">For women: BMR =9.247W + 3.098H - 4.330A + 447.593</p>
+          <p className="text-dark">
+            For men: BMR = 13.397W + 4.799H - 5.677A + 88.362
+          </p>
+          <p className="text-dark">
+            For women: BMR =9.247W + 3.098H - 4.330A + 447.593
+          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
--- client/src/pages/Profile/components/Calcul/InfoIdeal.tsx
+++ client/src/pages/Profile/components/Calcul/InfoIdeal.tsx
@@ -26,24 +26,39 @@ const InfoIdeal = () => {
       >
         <Image src={infoimg} height="20" width="20" />
       </Button>
-      <Modal show={show} onHide={handleClose} centered dialogClassName="modal-50w">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        centered
+        dialogClassName="modal-50w"
+      >
         <Modal.Header closeButton>
-          <Modal.Title>Infomations sur IMC</Modal.Title>
+          <Modal.Title>Infomations sur les poids ideals</Modal.Title>
         </Modal.Header>
         <Modal.Body className="text-dark">
           <p className="text-dark">
-            Le calculateur de poids idéal calcule les plages de poids idéal en fonction de la taille, du sexe et de l'âge. L’idée de trouver le poids
-            idéal à l’aide d’une formule est recherchée depuis longtemps par de nombreux experts.
+            Le calculateur de poids idéal calcule les plages de poids idéal en
+            fonction de la taille, du sexe et de l'âge. L’idée de trouver le
+            poids idéal à l’aide d’une formule est recherchée depuis longtemps
+            par de nombreux experts.
           </p>
           <p className="text-dark">
-            Notez que le poids corporel idéal n’est pas une mesure parfaite. Il ne prend pas en compte les pourcentages de graisse corporelle et de
-            muscle dans le corps d'une personne. Cela signifie qu'il est possible que des athlètes en bonne santé et en bonne forme physique soient
-            considérés comme en surpoids en fonction de leur poids corporel idéal. C'est pourquoi le poids corporel idéal doit être considéré dans la
-            perspective qu'il s'agit d'une mesure imparfaite et n'est pas nécessairement une indication de la santé, ni d'un poids vers lequel une
-            personne devrait nécessairement s'efforcer d'atteindre ; il est possible d'être au-dessus ou au-dessous de votre « poids idéal » et d'être
-            en parfaite santé.
+            Notez que le poids corporel idéal n’est pas une mesure parfaite. Il
+            ne prend pas en compte les pourcentages de graisse corporelle et de
+            muscle dans le corps d'une personne. Cela signifie qu'il est
+            possible que des athlètes en bonne santé et en bonne forme physique
+            soient considérés comme en surpoids en fonction de leur poids
+            corporel idéal. C'est pourquoi le poids corporel idéal doit être
+            considéré dans la perspective qu'il s'agit d'une mesure imparfaite
+            et n'est pas nécessairement une indication de la santé, ni d'un
+            poids vers lequel une personne devrait nécessairement s'efforcer
+            d'atteindre ; il est possible d'être au-dessus ou au-dessous de
+            votre « poids idéal » et d'être en parfaite santé.
           </p>
-          <p className="text-dark">Male: 56.2 kg + 1.41 kg per inch over 5 feet Female: 53.1 kg + 1.36 kg per inch over 5 feet</p>
+          <p className="text-dark">
+            Male: 56.2 kg + 1.41 kg per inch over 5 feet Female: 53.1 kg + 1.36
+            kg per inch over 5 feet
+          </p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
